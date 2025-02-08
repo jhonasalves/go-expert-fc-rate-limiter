c1696b71084835cbcfc04e19514a4a1d84644636
Jhonas Alves <jhonas.alvesm@gmail.com>
2025-02-08 09:48:10 -0300

    feat: add rate limiting middleware and configuration loading

---
+++ internal/middleware/ratelimiter/rate_limiter.go
@@ -0,0 +1,56 @@
+package ratelimiter
+
+import (
+	"net"
+	"net/http"
+	"sync"
+
+	"golang.org/x/time/rate"
+)
+
+type RateLimiter struct {
+	visitors map[string]*rate.Limiter
+	mu       sync.Mutex
+	rate     rate.Limit
+	burst    int
+}
+
+func NewRateLimiter(r rate.Limit, b int) *RateLimiter {
+	return &RateLimiter{
+		visitors: make(map[string]*rate.Limiter),
+		rate:     r,
+		burst:    b,
+	}
+}
+
+func (rl *RateLimiter) getIP(r *http.Request) string {
+	host, _, _ := net.SplitHostPort(r.RemoteAddr)
+	return host
+}
+
+func (rl *RateLimiter) getLimiter(ip string) *rate.Limiter {
+	rl.mu.Lock()
+	defer rl.mu.Unlock()
+
+	if limiter, exists := rl.visitors[ip]; exists {
+		return limiter
+	}
+
+	limiter := rate.NewLimiter(rl.rate, rl.burst)
+	rl.visitors[ip] = limiter
+
+	return limiter
+}
+
+func (rl *RateLimiter) RateLimitMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ip := rl.getIP(r)
+
+		if !rl.getLimiter(ip).Allow() {
+			http.Error(w, http.StatusText(http.StatusTooManyRequests), http.StatusTooManyRequests)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
--- internal/server/server.go
+++ internal/server/server.go
@@ -3,7 +3,10 @@ package webserver
 import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/jhonasalves/go-expert-fc-rate-limiter/configs"
 	"github.com/jhonasalves/go-expert-fc-rate-limiter/internal/handlers"
+	"github.com/jhonasalves/go-expert-fc-rate-limiter/internal/middleware/ratelimiter"
+	"golang.org/x/time/rate"
 )
 
 type Server struct {
@@ -11,10 +14,18 @@ type Server struct {
 }
 
 func NewServer() *Server {
+	configs, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+
 	r := chi.NewRouter()
 
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
+
+	rl := ratelimiter.NewRateLimiter(rate.Limit(configs.RateLimiterMaxIPRequests), configs.RateLimiterBurst)
+	r.Use(rl.RateLimitMiddleware)
 
 	r.Get("/", handlers.HomeHandler)
 
