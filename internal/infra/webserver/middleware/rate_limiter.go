def1538bab41759a6cd3873e6c39a3a241488d33
Jhonas Alves <jhonas.alvesm@gmail.com>
2025-02-22 12:55:59 -0300

    feat: enhance rate limiting middleware to support API key and improve request handling

--- internal/infra/webserver/middleware/rate_limiter.go
+++ internal/infra/webserver/middleware/rate_limiter.go
@@ -7,6 +7,8 @@ import (
 	"github.com/jhonasalves/go-expert-fc-rate-limiter/internal/pkg/ratelimiter"
 )
 
+const HeaderAPIKey = "API_KEY"
+
 type RateLimiterMiddleware struct {
 	limiter *ratelimiter.RateLimiter
 }
@@ -17,9 +19,19 @@ func NewRateLimiterMiddleware(l *ratelimiter.RateLimiter) *RateLimiterMiddleware
 
 func (rl *RateLimiterMiddleware) Handler(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
 		ip := rl.getIP(r)
+		token := rl.getToken(r)
 
-		if !rl.limiter.Allow(ip) {
+		var key string
+
+		if token != "" {
+			key = token
+		} else {
+			key = ip
+		}
+
+		if !rl.limiter.Allow(ctx, key) {
 			http.Error(w, "you have reached the maximum number of requests or actions allowed within a certain time frame", http.StatusTooManyRequests)
 			return
 		}
@@ -31,4 +43,8 @@ func (rl *RateLimiterMiddleware) Handler(next http.Handler) http.Handler {
 func (rl *RateLimiterMiddleware) getIP(r *http.Request) string {
 	host, _, _ := net.SplitHostPort(r.RemoteAddr)
 	return host
+}
+
+func (rl *RateLimiterMiddleware) getToken(r *http.Request) string {
+	return r.Header.Get(HeaderAPIKey)
 }
